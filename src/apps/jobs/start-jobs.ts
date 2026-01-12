6c286fd1c4b4ada4923a4c7b0c7c8efba8051f03
Igor Augusto Depiné Fiamoncini <igoradf@gmail.com>
2026-01-12 17:15:22 -0300

    Changed dropRemainingTestSchemas running time

--- src/apps/jobs/start-jobs.ts
+++ src/apps/jobs/start-jobs.ts
@@ -1,8 +1,8 @@
 import cron from "node-cron";
 import { dropRemainingTestSchemas } from "@/infra/database/helpers/test-running-functions.js";
 
-//At 12:00 PM every day
-cron.schedule("* * * * *", async () => {
+//At 12:00 every day
+cron.schedule("0 12 * * *", async () => {
 	await dropRemainingTestSchemas();
 });
 
